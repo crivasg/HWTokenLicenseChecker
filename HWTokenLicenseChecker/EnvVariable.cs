ff15f120a299b507e9c791c6ca19fad3fc4a26f0
Cesar A. Rivas <crivasg@gmail.com>
2012-08-06 21:23:47 -0400

    Created the name and value class members with thier setters and getters.

--- HWTokenLicenseChecker/EnvVariable.cs
+++ HWTokenLicenseChecker/EnvVariable.cs
@@ -7,11 +7,36 @@ namespace HWTokenLicenseChecker
 {
     class EnvVariable
     {
+        private String envName = @"";
+        private String envValue = @"";
 
         public EnvVariable()
         { 
         
         }
 
+        public String Name
+        {
+            set
+            {
+                envName = value;
+            }
+            get
+            {
+                return envName;
+            }
+        }
+
+        public String Value
+        {
+            set
+            {
+                envValue = value;
+            }
+            get
+            {
+                return envValue;
+            }
+        }
     }
 }
