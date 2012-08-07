251bf101b601317bd1f715d12aeb273822bcb5fe
Cesar A. Rivas <crivasg@gmail.com>
2012-08-07 19:36:39 -0400

    Removed Copyright from Assembly.cs and set file version to 0.1.3

--- HWTokenLicenseChecker/Properties/AssemblyInfo.cs
+++ HWTokenLicenseChecker/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HWTokenLicenseChecker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ITESM")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HWTokenLicenseChecker")]
-[assembly: AssemblyCopyright("Copyright © ITESM 2012")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
