cf5b1bfa897ff064883f7c1cf704945de1b436fe
Cesar A. Rivas <crivasg@gmail.com>
2012-08-07 21:56:44 -0400

    Fixed the format of the Prompt class

--- HWTokenLicenseChecker/Prompt.cs
+++ HWTokenLicenseChecker/Prompt.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 
 using System.Windows.Forms;
+using System.Drawing;
+
+//http://stackoverflow.com/questions/5427020/prompt-dialog-in-windows-forms
 
 namespace HWTokenLicenseChecker
 {
@@ -13,11 +16,21 @@ namespace HWTokenLicenseChecker
         {
             Form prompt = new Form();
             prompt.Width = 500;
-            prompt.Height = 100;
+            prompt.Height = 200;
             prompt.Text = caption;
-            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
-            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
-            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70 };
+
+            prompt.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text + @":",
+                Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold)
+            };
+            
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 ,
+                Height = 100, Font = new Font("Microsoft Sans Serif", 24, FontStyle.Regular)
+            };
+
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 125,
+                Height = 35, Font = new Font("Microsoft Sans Serif", 10, FontStyle.Regular) };
             confirmation.Click += (sender, e) => { prompt.Close(); };
             prompt.Controls.Add(confirmation);
             prompt.Controls.Add(textLabel);
