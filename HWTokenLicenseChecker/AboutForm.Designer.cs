cb380da0813f5a6fc92d0a6ce8fd7d7ee65479ae
Cesar A. Rivas <crivasg@gmail.com>
2013-03-02 21:44:14 -0500

    GUI fixes to the about box

--- HWTokenLicenseChecker/AboutForm.Designer.cs
+++ HWTokenLicenseChecker/AboutForm.Designer.cs
@@ -28,82 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.dismissAboutButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.dismissAboutButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.AutoSize = true;
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.dismissAboutButton, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 13);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.53881F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.46119F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(316, 329);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // dismissAboutButton
-            // 
-            this.dismissAboutButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.dismissAboutButton.Location = new System.Drawing.Point(120, 282);
-            this.dismissAboutButton.Name = "dismissAboutButton";
-            this.dismissAboutButton.Size = new System.Drawing.Size(75, 23);
-            this.dismissAboutButton.TabIndex = 0;
-            this.dismissAboutButton.Text = "Dismiss";
-            this.dismissAboutButton.UseVisualStyleBackColor = true;
-            this.dismissAboutButton.Click += new System.EventHandler(this.dismissAboutButton_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.richTextBox1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(310, 252);
-            this.groupBox1.TabIndex = 1;
+            this.groupBox1.Size = new System.Drawing.Size(317, 203);
+            this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "About";
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(7, 20);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 16);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(297, 226);
+            this.richTextBox1.Size = new System.Drawing.Size(311, 184);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
+            // 
+            // dismissAboutButton
+            // 
+            this.dismissAboutButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dismissAboutButton.Location = new System.Drawing.Point(133, 223);
+            this.dismissAboutButton.Name = "dismissAboutButton";
+            this.dismissAboutButton.Size = new System.Drawing.Size(75, 23);
+            this.dismissAboutButton.TabIndex = 2;
+            this.dismissAboutButton.Text = "Dismiss";
+            this.dismissAboutButton.UseVisualStyleBackColor = true;
+            this.dismissAboutButton.Click += new System.EventHandler(this.dismissAboutButton_Click);
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 354);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(341, 258);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dismissAboutButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "AboutForm";
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.Text = "About";
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button dismissAboutButton;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button dismissAboutButton;
+
     }
 }
