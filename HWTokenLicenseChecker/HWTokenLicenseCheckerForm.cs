bf9f818021677e4de65fe761984a916b0dcb040d
Cesar A. Rivas <crivasg@gmail.com>
2013-08-11 14:58:28 -0500

    Deleted SQL_QUERY_FOR_GRIDDATAVIEW  and replaced by Queries.FillDataGridView

--- HWTokenLicenseChecker/HWTokenLicenseCheckerForm.cs
+++ HWTokenLicenseChecker/HWTokenLicenseCheckerForm.cs
@@ -28,21 +28,6 @@ namespace HWTokenLicenseChecker
 
         private const String GITHUB_REPO_URL = @"https://github.com/crivasg/HWTokenLicenseChecker";
 
-        private const String SQL_QUERY_FOR_GRIDDATAVIEW =
-        @"SELECT LOWER(name) AS Username, host AS Hostname, MAX(used_licenses) AS Tokens, 
-    share_custom AS 'Custom String','' AS Type, login_time AS Date, feature_id as 'Feature Id' 
-    FROM user 
-    WHERE feature_id IN ( SELECT feature_id FROM feature WHERE isPartner = 0) AND isBorrow = 0 GROUP BY Username, Hostname 
-    UNION 
-    SELECT LOWER(name), host, MAX(used_licenses), share_custom,'HWPA',login_time AS Date, feature_id 
-    FROM user 
-    WHERE feature_id IN ( SELECT feature_id FROM feature WHERE isPartner != 0) AND isBorrow = 0 GROUP BY name, host, feature_id  
-    UNION 
-    SELECT LOWER(name), host, MAX(used_licenses), share_custom, 'BRRW', login_time AS Date, feature_id 
-    FROM user 
-    WHERE feature_id IN ( SELECT feature_id FROM feature WHERE isPartner = 0) AND isBorrow = 1 GROUP BY name, host 
-    ORDER BY Tokens DESC, Username ASC, Hostname ASC;";
-
         private int minHWPAFeatureId = -1;
         private int maxHWPAFeatureId = -2;
 
